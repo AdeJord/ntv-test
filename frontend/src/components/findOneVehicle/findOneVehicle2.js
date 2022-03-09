6669c6549fb04700a58fdac7c824f2fbedb4f453
Adrian Jordan <adejord@gmail.com>
2022-03-09 17:51:02 +0000

    added findOneVehilce 2 but has issues!

--- frontend/src/components/findOneVehicle/findOneVehicle2.js
+++ frontend/src/components/findOneVehicle/findOneVehicle2.js
@@ -1,95 +1,168 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState, useEffect} from "react";
-import axios from "axios";
- 
- 
-function SearchCar()
-{
-  const [search,setSearch] =useState('');
-  const [record,setRecord] = useState([]);
- 
-  
-    // On Page load display all records 
-    const loadEmployeeDetail = async () =>  
-    {
-      var response = fetch('http://localhost:5000/api/v1/employee')
-         .then(function(response){
-            return response.json();
-          })
-         .then(function(myJson) {
-            setRecord(myJson);
-          });
-    }
-    useEffect(() => {
-      loadEmployeeDetail();
-    }, []);
- 
-    // Search Records here 
-    const searchRecords = () =>
-    {
-        axios.get(`http://localhost:5000/api/v1/employee/searchRecord/${search}`)
-        .then(response => {
-          setRecord(response.data);
-        });
-         
-    }
- 
-    const loadRecordAgain = () =>
-    {
-      var response = fetch('http://localhost:5000/api/v1/employee')
-      .then(function(response){
-         return response.json();
-       })
-      .then(function(myJson) {
-         setRecord(myJson);
-       });
-         
-    }
-    useEffect(() => {
-      loadRecordAgain();
-    }, []);
- 
- 
-  return(
-    <section>  
-    <div class="container">  
-    <h4 className="mb-3 text-center mt-4">Search Records in MERN</h4>
-      <div class="row mt-3">
-      <div class="col-sm-11">
-        <div class="input-group mb-4 mt-3">
-          <div class="form-outline">
-           <input type="text" id="form1"    onKeyDown={loadRecordAgain} onKeyUp={searchRecords} onChange={(e)=>setSearch(e.target.value)} class="form-control" placeholder="Search Employee Here" style={{backgroundColor:"#ececec"}}/>
-        </div>
-        {/* <button type="button" onClick={searchRecords}  class="btn btn-success">
-            <i class="fa fa-search" aria-hidden="true"></i>
-        </button> */}
-        </div>  
-        <table class="table table-hover  table-striped table-bordered ml-4 ">
-            <thead>
-            <tr>
-                <th>Registration</th>
-                <th>Keys</th>
-                <th>Company</th>
-                <th>Comments</th>
-            </tr>
-            </thead>
-            <tbody>
-     
-            {record.map((Cars)=>
-                <tr>
-                <td>{Cars.regNo}</td>
-                <td>{Cars.keys}</td>
-                <td>{Cars.company}</td>
-                <td>{Cars.comments}</td>
-                </tr>
-                )} 
-            </tbody>
-        </table>
+import React from 'react';
+import { useRef, useState } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+//import styled from 'styled-components';
+import { getCarByRegistration } from '../../services/api';
+import { useParams } from "react-router-dom";
+
+/*
+I think I have to add the code here to get the reg number and use mongoose to search for it
+and return into the answer?
+*/
+
+const useStyles = makeStyles((theme) => ({
+  wrap: {
+    width: '100%',
+    height: '100vh',
+    backgroundColor: '#dddddd',
+    float: 'left'
+  },
+  results: {
+    padding: '10px',
+    margin: '50px',
+    width: '250px',
+    height: '250px',
+    backgroundColor: '#aaaa',
+    color: 'black',
+    borderWidth: '4px',
+    borderColor: 'black',
+    borderRadius: '10px',
+    textAlign: 'left',
+    fontSize: '20px',
+  },
+  content: {
+    fontSize: '40px',
+    width: 'calc(100% - 40px)',
+    height: 'auto',
+    padding: '20px',
+    backgroundColor: '#cccccc',
+    float: 'left'
+  },
+  submitBtn: {
+    fontSize: '20px',
+    width: '100px',
+    height: 'auto',
+    padding: '15px',
+    backgroundColor: '#cccccc',
+    borderRadius: '10px',
+    paddingBottom: '10px',
+    marginLeft: '20px',
+    marginBottom: '30px'
+  },
+  form: {
+    fontSize: '20px',
+    width: '100px',
+    height: 'auto',
+    padding: '15px',
+    borderRadius: '10px',
+    paddingBottom: '10px'
+  },
+  input: {
+    fontSize: '30px',
+    height: '75px',
+    width: '300px',
+    margin: '20px',
+    backgroundColor: 'yellow',
+    color: 'black',
+    borderWidth: '4px',
+    borderColor: 'black',
+    borderRadius: '10px',
+    textAlign: 'center',
+    textTransform: 'upperCase'
+  }
+}));
+
+/*const Container = styled.div`
+display: flex;
+flex-direction: column
+`*/
+
+export default function FindOneVehicle2() {
+  const classes = useStyles();
+  const [getCar, setCar] = useState([]);
+  const [val, setVal] = useState('');
+  const inputRef = useRef();
+
+
+  //little submit button (Correct way but not working, 1st says nothing, and second click works ok)
+// after 1st click, updates 'Submit Value' which is val, 
+// then opens alert box saying nothing on that reg
+// click OK then updates console with LB Cars matching 0 & LB
+// click Submit again and it works LB Cars matching 1 and LB RDA877L, but warning appears about keys
+
+
+  async function submitHandler (e)  {
+    e.preventDefault();
+    await setVal(inputRef.current.value);
+
+    await getCarByRegistration(val).then(response => {
+      //alert(JSON.stringify(response.data, null, 2));
+      if (response.data.length > 0) {
+        setCar(response.data);
+      } else {
+        alert('LB Nothing on that reg');
+      }
+      console.log(`LB Cars matching ${val}:-  ${response.data.length}`)
+    });
+
+    console.log(`LB ${val}`)
+  }
+
+
+//     *****************************Big Button works, so does little button but little button is a step behind**********************
+
+
+  //Big submit button (Wrong way but working)
+  async function handleClick() {
+    let searchReg = document.getElementById('regInput');
+    await getCarByRegistration(searchReg.value).then(response => {
+      //alert(JSON.stringify(response.data, null, 2));
+      if (response.data.length > 0) {
+        setCar(response.data);
+      } else {
+        alert('BB Nothing on that reg');
+      }
+      console.log(`BB Cars matching ${val}:-  ${response.data.length}`)
+      console.log(`BB ${val}`)
+    });
+  };
+
+  return (
+    <div className={classes.wrap}>
+      <div className={classes.content}>
+        Search by Reg Number
       </div>
+      <form>
+        <input type="text" name="regInput" id="regInput" className={classes.input} placeholder="Enter Reg Here" />
+      </form>
+      <div className="App">
+      </div>
+      <button onClick={handleClick} className={classes.submitBtn}>Submit</button>
+      <form onSubmit={submitHandler}>
+        <input ref={inputRef} />
+        <button type="submit">Submit</button>
+      </form>
+      <p>Submit Value: <b>{val}</b></p>
+      <div id="resultsContainer" className={classes.results}>
+        {getCar.length > 0 &&
+          getCar.map(car => {
+            return (
+              <div className={classes.car}>
+                <div className={classes.detail}>Registration: {car.regNo}</div>
+                <div className={classes.detail}>Keys: {car.keys}</div>
+                <div className={classes.detail}>Company: {car.company}</div>
+                <div className={classes.detail}>Comments: {car.comments}</div>
+              </div>
+            )
+          })}
+        {getCar.length === 0 &&
+          <p>Results</p>
+        }
       </div>
     </div>
-   </section>
-  )
+
+
+  );
 }
- 
-export default SearchCar;
+
+
